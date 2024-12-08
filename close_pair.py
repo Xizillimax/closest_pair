17cb1a036848d84c583afdd4562d1edb5b055c99
Максим Зеленин <Maxim.Z.3105@gmail.com>
2024-12-08 17:20:01 +0300

    Worked

---
+++ close_pair.py
@@ -0,0 +1,23 @@
+def closest_pair_of_points(points: list[tuple[float, float]]) -> tuple[float, tuple[float, float], tuple[float, float]] | None:
+    """Находит ближайшую пару точек в массиве"""
+    if len(points) < 2:
+        return None
+
+    min_distance: float = float('inf')
+    closest_pair: tuple[float, tuple[float, float], tuple[float, float]] | None = None
+
+    for i in range(len(points)):
+        for j in range(i + 1, len(points)):
+            distance = (sum((p_coord - q_coord) ** 2.0 for p_coord, q_coord in zip(points[i], points[j]))) ** 0.5
+            if distance < min_distance:
+                min_distance = distance
+                closest_pair = (min_distance, points[i], points[j])
+
+    return closest_pair
+
+# Примеры использования
+
+
+assert closest_pair_of_points([(1, 1), (2, 2), (1, 2)]) == (1, (1, 1), (1, 2))
+
+print(closest_pair_of_points([(1, 1), (2, 2), (1, 2)]))
